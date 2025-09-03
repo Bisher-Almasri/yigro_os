4509ea45fc86709d77499b080c10d4566df8cad6
Bisher Almasri <bbgyttv@gmail.com>
2025-09-03 16:24:17 -0400

    added printing and started tests, planinng kb later

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,58 @@
 #![no_std]
 #![no_main]
+#![reexport_test_harness_main = "test_main"]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
+mod vga_buf;
 
-
-static MSG: &[u8] = b"Test!";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buf = 0xb8000 as *mut u8;
+    println!("skib{}", "idi");
 
-    for (i, &byte) in MSG.iter().enumerate() {
-         unsafe {
-            *vga_buf.offset(i as isize * 2) = byte; // text
-            *vga_buf.offset(i as isize * 2 + 1) = 0xb; // color
-        }
-    }
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 // since we are using nostd we need ot make our wn panic handler
 // this is called on panic
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("skibidi test count: {}", tests.len());
+
+    for test in tests {
+        test();
+    }
+}
+
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum QemuExitCode {
+    
 }
